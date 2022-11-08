76d043635fb4083c16a884cd6fe7b1d75aac350a
shiruzo <92055959+shiruzo@users.noreply.github.com>
2022-11-07 20:42:18 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new Drivetrain. */
+  private final CANSparkMax spark_1;
+  public Drivetrain() {
+      spark_1 = new CANSparkMax(0, MotorType.kBrushless);
+  }
+  public void sparkLeft() {
+    spark_1.set(-0.05);
+  }
+  public void sparkRight() {
+    spark_1.set(0.05);
+  }  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
