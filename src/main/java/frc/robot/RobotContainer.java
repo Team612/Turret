2a333d385aaa7d93ed03497634b271f616df6eab
shiruzo <92055959+shiruzo@users.noreply.github.com>
2022-11-09 15:46:22 -0500

    Add files via upload

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.TurnLeft;
-import frc.robot.commands.TurnRight;
+import frc.robot.commands.TurnTurret;
 import frc.robot.controls.ControlMap;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -39,9 +37,9 @@ public class RobotContainer {
   }
 
   private void turretButtonBindings() {
-    ControlMap.GUNNER_A.whenPressed(new TurnLeft(m_drivetrain));
-    ControlMap.GUNNER_B.whenPressed(new TurnRight(m_drivetrain));
-
+    ControlMap.GUNNER_A.whenPressed(new TurnTurret(m_drivetrain, -0.05));
+    ControlMap.GUNNER_B.whenPressed(new TurnTurret(m_drivetrain, 0.05));
+    ControlMap.GUNNER_X.whenPressed(new TurnTurret(m_drivetrain, 0));
   }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -50,6 +48,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new TurnLeft(m_drivetrain);
+    return new TurnTurret(m_drivetrain, 0);
   }
 }
