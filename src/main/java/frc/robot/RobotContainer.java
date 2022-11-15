1be9cd43288a7dedc0f0a24ff9c59b4265949fec
noahtylee <noahtylee@gmail.com>
2022-11-14 19:06:21 -0500

    sendable chooser added

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.TurnTurret;
 import frc.robot.commands.Vision;
 import frc.robot.controls.ControlMap;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,6 +24,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public Drivetrain m_drivetrain = new Drivetrain();
+  private TurnTurret m_left = new TurnTurret(m_drivetrain, -0.05);
+  private TurnTurret m_right = new TurnTurret(m_drivetrain, 0.05);
+  private TurnTurret m_stop = new TurnTurret(m_drivetrain, 0);
+  private Vision m_vision = new Vision(m_drivetrain);
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -34,14 +42,21 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    m_chooser.addOption("Turn Left", m_left);
+    m_chooser.addOption("Turn Right", m_right);
+    m_chooser.addOption("Stop look both ways, passenger has right of way", m_stop);
+    m_chooser.addOption("Vision", m_vision);
+    SmartDashboard.putData(m_chooser);
+
     turretButtonBindings();
   }
 
   private void turretButtonBindings() {
-    ControlMap.GUNNER_A.whenPressed(new TurnTurret(m_drivetrain, -0.05));
-    ControlMap.GUNNER_B.whenPressed(new TurnTurret(m_drivetrain, 0.05));
+    ControlMap.GUNNER_A.whenPressed(m_left);
+    ControlMap.GUNNER_B.whenPressed(m_right);
     ControlMap.GUNNER_X.whenPressed(new TurnTurret(m_drivetrain, 0));
   }
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -49,6 +64,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new Vision(m_drivetrain);
+    return m_chooser.getSelected();
   }
 }
