02bf12edd3e2a78b1e7f15e7f892fcd3477a778b
vision <chantilly.612.programming@gmail.com>
2022-11-16 15:28:38 -0500

    updated code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import frc.robot.commands.Vision;
 import frc.robot.controls.ControlMap;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,10 +23,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public Drivetrain m_drivetrain = new Drivetrain();
-  private TurnTurret m_left = new TurnTurret(m_drivetrain, -0.05);
-  private TurnTurret m_right = new TurnTurret(m_drivetrain, 0.05);
-  private TurnTurret m_stop = new TurnTurret(m_drivetrain, 0);
-  private Vision m_vision = new Vision(m_drivetrain);
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,18 +37,12 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    m_chooser.addOption("Turn Left", m_left);
-    m_chooser.addOption("Turn Right", m_right);
-    m_chooser.addOption("Stop look both ways, passenger has right of way", m_stop);
-    m_chooser.addOption("Vision", m_vision);
-    SmartDashboard.putData(m_chooser);
-
     turretButtonBindings();
   }
 
   private void turretButtonBindings() {
-    ControlMap.GUNNER_A.whenPressed(m_left);
-    ControlMap.GUNNER_B.whenPressed(m_right);
+    ControlMap.GUNNER_A.whenPressed(new TurnTurret(m_drivetrain, -0.05));
+    ControlMap.GUNNER_B.whenPressed(new TurnTurret(m_drivetrain, 0.05));
     ControlMap.GUNNER_X.whenPressed(new TurnTurret(m_drivetrain, 0));
   }
 
